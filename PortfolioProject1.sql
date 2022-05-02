5cff12847c997ba452daa2f4f49bbef31016ea74
ShabazSJ <39992191+ShabazSJ@users.noreply.github.com>
2022-05-02 12:58:59 +0530

    Update PortfolioProject1.sql

--- PortfolioProject1.sql
+++ PortfolioProject1.sql
@@ -1,3 +1,8 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 SELECT*
 	FROM PortfolioProject..CovidDeaths
 	WHERE Continent is not null	
@@ -7,6 +12,8 @@ SELECT*
 	--SELECT*
 	--FROM PortfolioProject..CovidVaccinations
 	--ORDER BY 3,4
+	
+	-- Select Data that we are going to be starting with
 
 	SELECT Location,date,total_cases,new_cases,total_deaths,population
 	FROM PortfolioProject..CovidDeaths
@@ -20,15 +27,15 @@ SELECT*
 	WHERE location like 'india'
 	ORDER BY 1,2
 
-	--Looking at Toatal cases vs Population
-	--Shows what percentage of Population got Covid
+	--Total cases vs Population
+	--Shows what percentage of Population infected with Covid
 
 	SELECT Location,date,total_cases,Population,(total_cases/Population)*100 AS PercentPopulationInfected
 	FROM PortfolioProject..CovidDeaths
 	--WHERE location like 'india'
 	ORDER BY 1,2
 
-	--Looking at countries with Highest infection rate compared to population
+	--Countries with Highest infection rate compared to population
 
 		SELECT Location,Population,MAX(total_cases) as HighestInfectionCount,MAX((total_cases/Population))*100 AS PercentPopulationInfected
 	FROM PortfolioProject..CovidDeaths
@@ -37,7 +44,7 @@ SELECT*
 	ORDER BY PercentPopulationInfected desc
 
 
-	--Looking at countries with Highest Death Count per Population
+	--countries with Highest Death Count per Population
 
 	SELECT Location,MAX(cast(Total_deaths as int)) as TotalDeathCount
 	FROM PortfolioProject..CovidDeaths
@@ -46,7 +53,7 @@ SELECT*
 	Group by Location
 	ORDER BY TotalDeathCount desc
 
-	--LETS BREAK THINGS DOWN BY CONTINENT
+	--BREAKING THINGS DOWN BY CONTINENT
 
 	--Showing continents with the highest death count per population
 
@@ -68,7 +75,7 @@ SELECT*
 	--Group by date
 	ORDER BY 1,2
 
-	--Looking at Total Population vs Vaccinations
+	--Total Population vs Vaccinations
 
 SELECT *
 	FROM PortfolioProject..CovidDeaths dea
@@ -88,7 +95,7 @@ SELECT *
 		WHERE dea.continent is not null
 		Order by 2,3
 
---USE CTE
+-- Using CTE to perform Calculation on Partition By in previous query
 
 With PopvsVac (Continent,location,Date,Population,New_Vaccinations,RollingPeopleVaccinated)
 as
@@ -108,7 +115,7 @@ SELECT dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
 		FROM PopvsVac
 		
 
---TEMP TABLE
+-- Using Temp Table to perform Calculation on Partition By in previous query
 
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
